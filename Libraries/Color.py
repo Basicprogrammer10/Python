1bb23f95d9b5891d6c348d270044960327605dcf
Connor Slade <turtleslade@gmail.com>
2019-12-16 16:24:58 -0500

    Adding to Color Lib

--- Libraries/Color.py
+++ Libraries/Color.py
@@ -1,3 +1,4 @@
+import sys
 RED     = "\033[1;31m"  
 BLUE    = "\033[1;34m"
 CYAN    = "\033[1;36m"
@@ -5,3 +6,14 @@ GREEN   = "\033[0;32m"
 RESET   = "\033[0;0m"
 BOLD    = "\033[;1m"
 REVERSE = "\033[;7m"
+def color(x):
+    sys.stdout.write(x)
+
+def cprint(text, color):
+    sys.stdout.write(color)
+    print(text)
+    sys.stdout.write(RESET)
+
+# Example
+# sys.stdout.write(RED)
+# cprint("TEST", RED)
