c55876c403e1aebba49efcc5eae5173ae5cdeee2
Connor Slade <turtleslade@gmail.com>
2019-12-16 17:10:22 -0500

    Changed Saveing

--- Perfect Number Finder/Perfect Number Finder.py
+++ Perfect Number Finder/Perfect Number Finder.py
@@ -1,9 +1,13 @@
 import sys
 from Color import *
-file = open('PerfectNumbers.txt', 'w')
+file = open('PerfectNumber.txt', 'w')
+file.write("Perfect Number Finder V3\n")
+file.write("Made By Connor Slade\n")
+file.write("-------------------------\n")
+file.close()
 MN = int(input("Min: "))
 MA = int(input("Max: "))
-for x in range(MN,MA):
+for x in range(MN, MA):
     a = []
     t = 0
     for i in range(1, x + 1):
@@ -11,13 +15,16 @@ for x in range(MN,MA):
             a.append(i)
     a.remove(x)
     for i in range(len(a)):
-       t = t + a[i]
+        t = t + a[i]
     if x == t:
+        file = open('PerfectNumber.txt', 'a')
         file.writelines(str(x))
         file.write("\n")
-file.close()
+        file.close()
 
-file = open('PerfectNumbers.txt', 'r')
+file = open('PerfectNumber.txt', 'r')
 l = file.readlines()
-color(GREEN)
-print(l)
+l.remove("Perfect Number Finder V3\n")
+l.remove("Made By Connor Slade\n")
+l.remove("-------------------------\n")
+cprint(l, GREEN)
