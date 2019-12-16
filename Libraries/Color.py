3b1f85b2db993b70ad62aeee9f2fb197d8af6a73
Connor Slade <turtleslade@gmail.com>
2019-12-16 16:44:18 -0500

    Changing cprint

--- Libraries/Color.py
+++ Libraries/Color.py
@@ -7,13 +7,15 @@ RESET   = "\033[0;0m"
 BOLD    = "\033[;1m"
 REVERSE = "\033[;7m"
 def color(x):
+    '''Changs the color of the text printed to terminal'''
     sys.stdout.write(x)
 
 def cprint(text, color):
+    '''Prints text to terminal in color'''
     sys.stdout.write(color)
     print(text)
     sys.stdout.write(RESET)
-
+ 
 # Example
 # sys.stdout.write(RED)
 # cprint("TEST", RED)
