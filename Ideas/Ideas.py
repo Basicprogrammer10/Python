8c12e496ffc7f0fe2319f49210248814e5ea84c5
Connor Slade <turtleslade@gmail.com>
2019-12-19 06:39:20 -0500

    Fixes delete line

--- Ideas/Ideas.py
+++ Ideas/Ideas.py
@@ -1,20 +1,32 @@
 import random
 from Color import *
-file = open("ideas.txt", "r+")
-f = file.read()
+file = open("ideas.txt", "r")
+lines = file.readlines()
+print("Lines:", lines)
+f = open("ideas.txt", "r").read()
 g = f.split("\n")
+print("file.read:", f)
+print("f.split:", g)
+
+
 def main():
     r = random.randint(0, len(g)-1)
+    c = g[r]
+    print("C is", c)
     print(g[r])
     x = input("Good Idea? ")
     if x == "Y":
         cprint(g[r], GREEN)
-        for i in range(len(g)):
-            if i != g[r]:
-                f.seek(r, 0)
-                file.write('')
-        f.truncate()
+        file = open("ideas.txt", "w")
+        file.seek(0)
+        for i in range(len(g)-1):
+            print("I is", i)
+            if i != c:
+                file.write(g[i])
+        open("ideas.txt", "r+").truncate()
         exit()
     else:
         main()
+
+
 main()
