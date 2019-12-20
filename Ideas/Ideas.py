9dacb3d8da73b877dd6f7c319deb91a088116f15
Connor Slade <turtleslade@gmail.com>
2019-12-19 21:50:27 -0500

    Update Ideas.txt

--- Ideas/Ideas.py
+++ Ideas/Ideas.py
@@ -2,25 +2,25 @@ import random
 from Color import *
 file = open("ideas.txt", "r")
 lines = file.readlines()
-print("Lines:", lines)
+#print("Lines:", lines)
 f = open("ideas.txt", "r").read()
 g = f.split("\n")
-print("file.read:", f)
-print("f.split:", g)
+#print("file.read:", f)
+#print("f.split:", g)
 
 
 def main():
     r = random.randint(0, len(g)-1)
     c = g[r]
-    print("C is", c)
-    print(g[r])
+    #print("C is", c)
+    cprint(g[r], CYAN)
     x = input("Good Idea? ")
     if x == "Y":
         cprint(g[r], GREEN)
         file = open("ideas.txt", "w")
         file.seek(0)
         for i in range(len(g)-1):
-            print("I is", i)
+            #print("I is", i)
             if i != c:
                 file.write(g[i] + "\n")
         open("ideas.txt", "r+").truncate()
