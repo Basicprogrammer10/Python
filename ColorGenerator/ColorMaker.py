43b8c947950aa8ca246fcb9b0e0feb8507ebfd01
Connor Slade <turtleslade@gmail.com>
2020-02-27 22:52:48 -0500

    Did Somthing

--- ColorGenerator/ColorMaker.py
+++ ColorGenerator/ColorMaker.py
@@ -1,14 +1,16 @@
 import random
 colors = []
-file = open('Nouns.txt', 'r')
+
+file = open('ColorGenerator/Nouns.txt', 'r')
 noun = file.readlines()
 file.close()
-file = open('Colors.txt', 'r')
+
+file = open('ColorGenerator/Colors.txt', 'r')
 color = file.readlines()
 file.close()
 
 file = open('NewColors.txt', 'w')
-for i in range(1000000):
+for i in range(10):
     print(i)
     if str(noun[random.randint(0,len(noun)-1)]) + str(color[random.randint(0,len(color)-1)]) not in colors:
         file.write(str(noun[random.randint(0,len(noun)-1)]) + str(color[random.randint(0,len(color)-1)]))
