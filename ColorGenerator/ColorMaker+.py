0a583ee95de3902d0aee6a348f652765c7ae54b0
Connor Slade <turtleslade@gmail.com>
2020-02-28 00:02:10 -0500

    i did a thing

--- ColorGenerator/ColorMaker+.py
+++ ColorGenerator/ColorMaker+.py
@@ -1,12 +1,5 @@
-import random
+import random, tqdm
 colors = []
-def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
-    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-    filledLength = int(length * iteration // total)
-    bar = fill * filledLength + '-' * (length - filledLength)
-    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = printEnd)
-    if iteration == total: 
-        print()
 num = int(input("How many Color Names would you like to make? "))
 file = open('ColorGenerator/Nouns.txt', 'r')
 noun = file.readlines()
@@ -15,7 +8,7 @@ file = open('ColorGenerator/Colors.txt', 'r')
 color = file.readlines()
 file.close()
 file = open('ColorGenerator/NewColors.txt', 'w')
-for i in range(num):
+for i in tqdm(range(num)):
     word = str(noun[random.randint(0,len(noun)-1)]) + str(color[random.randint(0,len(color)-1)])
     if word not in colors:
         file.write(word)
@@ -23,6 +16,5 @@ for i in range(num):
         file.write("----------\n")
     else:
         i = i + 1
-    printProgressBar(i, num)
 file.write("Color Name Gen. V2 | By Connor Slade")
 file.close()
