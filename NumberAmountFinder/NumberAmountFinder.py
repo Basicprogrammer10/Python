96f877591161e3b6abbb0db6d7a7ae28f68423b6
Connor Slade <turtleslade@gmail.com>
2020-03-09 17:40:04 -0400

    Worked on Stat Finder

--- NumberAmountFinder/NumberAmountFinder.py
+++ NumberAmountFinder/NumberAmountFinder.py
@@ -1,20 +1,22 @@
+from statistics import median
 num = [1, 1, 5, 3, 3, 3]
 final = []
 low = 0
 high = 0
-
 for i in range(len(num)):
-    if num[i]>high:
+    if num[i] > high:
         high = num[i]
-
 low = high
 for i in range(len(num)):
-    if num[i]<low:
+    if num[i] < low:
         low = num[i]
-
-for i in range(1, len(num)):
-    if i == num[i]:
-        final.append(num[i])
-        
-
-print(final)
+num.sort()
+print(num)
+# prints the Min of the dataset to the Command Line
+print("Min:    " + str(low))
+# prints the Max of the dataset to the Command Line
+print("Max:    " + str(high))
+# print("Q1:     " + str(int())) #prints Q1 to the Command Line
+# prints the median to the Command Line
+print("Median: " + str(int(median(num))))
+# print("Q3:     " + str(int())) #prints Q3 to the command Line
