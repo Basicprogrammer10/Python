fa10c0664c88322aaabeed857c2f3aa4683c7b4f
Connor Slade <turtleslade@gmail.com>
2020-03-10 23:58:34 -0400

    Did some Graphing

---
+++ Data/Bar.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = [2,5,70,40,30,45,50,45,43,40,44,60,7,13,57,18,90,77,32,21,20,40] 
+
+plt.bar(data, data)
+#plt.xticks(y_pos, objects)
+plt.show()
--- Data/Hist.py
+++ Data/Hist.py
@@ -1,9 +1,9 @@
 import numpy as np; np.random.seed(13)
 import matplotlib.pyplot as plt
-
-
+plt.style.use('fivethirtyeight')
 data = [26, 30, 22, 15, 21, 24, 28, 32, 24, 25, 18, 35]
+bins = len(data)
 
-plt.hist(data, ec="k")
+plt.hist(data, bins=bins, edgecolor='black')
 
 plt.show()
