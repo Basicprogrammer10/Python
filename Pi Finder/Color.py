258e846deee52a36b591521d02827817257ad528
Basicprogrammer10 <turtleslade@gmail.com>
2020-04-06 17:45:21 -0400

    changen it up

---
+++ Pi Finder/Color.py
@@ -0,0 +1,34 @@
+import sys
+RED = "\033[1;31m"
+BLUE = "\033[1;34m"
+CYAN = "\033[1;36m"
+GREEN = "\033[0;32m"
+RESET = "\033[0;0m"
+BOLD = "\033[;1m"
+REVERSE = "\033[;7m"
+def color(x):
+    '''Changs the color of the text printed to terminal'''
+    RED = "\033[1;31m"
+    BLUE = "\033[1;34m"
+    CYAN = "\033[1;36m"
+    GREEN = "\033[0;32m"
+    RESET = "\033[0;0m"
+    BOLD = "\033[;1m"
+    REVERSE = "\033[;7m"
+    sys.stdout.write(x)
+def cprint(text, color):
+    '''Prints text to terminal in color'''
+    RED = "\033[1;31m"
+    BLUE = "\033[1;34m"
+    CYAN = "\033[1;36m"
+    GREEN = "\033[0;32m"
+    RESET = "\033[0;0m"
+    BOLD = "\033[;1m"
+    REVERSE = "\033[;7m"
+    sys.stdout.write(color)
+    print(text)
+    sys.stdout.write(RESET)
+
+# Example
+# sys.stdout.write(RED)
+# cprint("TEST", RED)
--- Pi Finder/PiFinderSaveShow.py
+++ Pi Finder/PiFinderSaveShow.py
@@ -1,9 +1,27 @@
 from datetime import datetime
-from Color import *
-import decimal
+import decimal, sys
+RED = "\033[1;31m"
+BLUE = "\033[1;34m"
+CYAN = "\033[1;36m"
+GREEN = "\033[0;32m"
+RESET = "\033[0;0m"
+BOLD = "\033[;1m"
+REVERSE = "\033[;7m"
+
+
+def cprint(text, color):
+    '''Prints text to terminal in color'''
+    sys.stdout.write(color)
+    print(text)
+    sys.stdout.write(RESET)
+
+
 file = open('Pi.txt', 'w')
 file.write("Pi Finder V2\n")
 file.write("Made By Connor Slade\n")
+start = datetime.now()
+
+
 def pi():
     decimal.getcontext().prec += 2  # extra digits for intermediate steps
     three = decimal.Decimal(3)      # substitute "three=3.0" for regular floats
@@ -17,6 +35,8 @@ def pi():
         cprint(s, RED)
     decimal.getcontext().prec -= 2
     return +s               # unary plus applies the new precision
+
+
 x = int(input("precision: "))
 file.write("Precision: " + str(x) + "\n")
 file.write("-------------------------\n")
@@ -24,5 +44,6 @@ decimal.getcontext().prec = x
 pi = pi()
 file.write(str(pi) + "\n")
 file.write("-------------------------\n")
+file.write("Started at " + str(start))
 file.write("Finished at " + str(datetime.now()))
 cprint(pi, GREEN)
