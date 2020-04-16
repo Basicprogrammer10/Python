dde86770d794b89e1d06c6b13cd492e406af0e7a
Basicprogrammer10 <turtleslade@gmail.com>
2020-04-16 12:38:03 -0400

    I did a bad thing

--- MC/deaths.py
+++ MC/deaths.py
@@ -1,25 +1,34 @@
 import json
 import os,glob
 folder_path = 'N:\Minecraft\SalC1\world\stats'
+#folder_path = 'N:\Minecraft\SalC1\jsonTEST'
 total = 0
 players = 0
 death = [];
+bdeath = []
 for filename in glob.glob(os.path.join(folder_path, '*.json*')):
     with open(filename, 'r') as data:
         obj = json.loads(data.read())
         try:
             total = total + obj['stat.deaths']
             players = players + 1
+            bdeath.append(obj['stat.deaths'])
+            death.append(data)
             death.append(obj['stat.deaths'])
         except:
             players = players + 1
+            bdeath.append(0)
+            death.append(data)
             death.append(0)
 file = open('MC\MCdeath.txt', 'w')
 file.write(str(death) + "\n")
 file.write("---------------\n")
 file.write("Total server deaths: " + str(total) + "\n")
 file.write("Average: " + str(total/players) + "\n")
+file.write("Max: " + str(max(bdeath)) + "\n")
+file.write("Min: " + str(min(bdeath)) + "\n")
 file.close()
-print(death)
 print("Total server deaths: " + str(total))
 print("Average: " + str(total/players))
+print("Max: " + str(max(bdeath)) + "\n")
+print("Min: " + str(min(bdeath)) + "\n")
--- Test/ai.py
+++ Test/ai.py
@@ -1,1 +1,3 @@
+test = [10, 6, 5]
 print("ai is hard... :(")
+print(max(test))
