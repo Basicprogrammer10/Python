52eb93e0b16c56c519f2e2920ed49a43060f6239
Basicprogrammer10 <turtleslade@gmail.com>
2020-04-29 00:17:56 -0400

    My manager said so

--- IdeaGen/IdeaGUI.py
+++ IdeaGen/IdeaGUI.py
@@ -1,5 +1,6 @@
-import random, easygui
+import random, easygui, sys
 filepre = "IdeaGen/"
+
 def list(num):
     '''Creates a file with some Ideas!'''
     colors = []
@@ -28,9 +29,9 @@ def showideas():
     f.close()
     easygui.codebox(text)
 
-def goodideas():
+def goodideas(num):
+    '''Does some Nosetastic Stuff!!'''
     colors = []
-    num = int(input("How many Entrepreneur Opportunities would you like to make? "))
     file = open('IdeaGen/PGNouns.txt', 'r')
     noun = file.readlines()
     file.close()
@@ -38,14 +39,28 @@ def goodideas():
     color = file.readlines()
     file.close()
     file = open('IdeaGen/GoodishNewIdeas.txt', 'w')
+    choices = ["Yes", "No", "Exit"]
     for i in range(num):
         word = str(noun[random.randint(0,len(noun)-1)]) + str(color[random.randint(0,len(color)-1)])
         if word not in colors:
-            if input("Is\n"+word+" A Good Idea?") == "Y":
+            opt = easygui.buttonbox("Is\n"+word+" A Good Idea?", choices=choices)
+            if opt == "Yes":
                 file.write(word)
                 colors.append(word)
                 file.write("----------\n")
+        elif opt == "Exit":
+            pass
         else:
             i = i + 1
+        if opt == "Exit":
+            break
     file.write("Entrepreneur Opportunities Gen. V1 | By Connor Slade")
     file.close()
+
+while True:
+    choices = ["Yes", "No", "Quit"]
+    opt = easygui.buttonbox("Chose an option", choices=choices)
+    if opt == "Quit":
+        quit()
+    elif opt == "List":
+        list()
--- Test/EasyGui.py
+++ Test/EasyGui.py
@@ -1,7 +1,9 @@
 import easygui
 me = ["hello", "World"]
 #print(easygui.integerbox("How many?"))
-easygui.ynbox('Shall I continue?', 'Title', ('Yes', 'No'))
+#easygui.ynbox('Shall I continue?', 'Title', ('Yes', 'No'))
 #easygui.msgbox('This is a basic message box.', 'Title Goes Here')
 #print(easygui.buttonbox('Click on your favorite flavor.', 'Favorite Flavor', ('Chocolate', 'Vanilla', 'Strawberry')))
 #easygui.textbox()
+
+print(easygui.ynbox("Is\n"+" A Good Idea?", 'Idea', ('Yes', 'No')))
