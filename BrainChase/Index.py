65a04f0cde012c5e7ab7ffd5a8869e66430f6f8b
Basicprogrammer10 <turtleslade@gmail.com>
2020-07-16 20:26:41 -0400

    V3.17

--- BrainChase/Index.py
+++ BrainChase/Index.py
@@ -8,8 +8,8 @@ import os
 import urllib.request
 from pathlib import Path
 from random import randint, shuffle
-#ver = 1
-ver = 3.16
+ver = 3.17
+ver_date = '07162020'
 try:
     test = filepath
 except:
@@ -64,8 +64,7 @@ def colored(text, color):
 
 
 if args["version"] != False:
-    print(colored("BrainChaseSolver™", 'blue') + " | " + colored("V" +
-                                                                 str(ver), 'yellow') + " | " + colored("By: Connor Slade", 'green'))
+    print(colored("BrainChaseSolver™", 'blue') + " | " + colored("V" + str(ver), 'yellow') + " | " + colored(ver_date[0:2] + '/' + ver_date[2:4] + '/' + ver_date[4:8] ,'magenta') + " | " + colored("By: Connor Slade", 'green'))
 
 elif args["duck"] != None:
     print(colored("https://duckduckgo.com/?q=" + args["duck"], 'cyan'))
@@ -80,28 +79,29 @@ elif args["update"] != False:
             update_data = urllib.request.urlopen("https://raw.githubusercontent.com/Basicprogrammer10/Python/master/BrainChase/BrainChase.version")
             config.read_string(update_data.read().decode())
             new_version = config.get('version_info', 'new_version')
+            new_version_date = config.get('version_info', 'update_date')
             if float(ver) == float(new_version):
                 print(colored("BrainChaseSolver™ ", "blue") + colored("is up to date!", 'green'))
-                print(colored("=====================", 'magenta'))
-                print(colored("Current Version: ", 'blue') + colored(ver,'cyan'))
-                print(colored("Newest Version:  ", 'blue') + colored(new_version,'cyan'))
+                print(colored("===================================", 'magenta'))
+                print(colored("Current Version: ", 'blue') + colored(ver,'cyan') + colored("  (" + ver_date[0:2] + '/' + ver_date[2:4] + '/' + ver_date[4:8] + ")",'magenta'))
+                print(colored("Newest Version:  ", 'blue') + colored(new_version,'cyan') + colored("  (" + new_version_date[0:2] + '/' + new_version_date[2:4] + '/' + new_version_date[4:8] + ")",'magenta'))
             elif float(ver) > float(new_version):
                 print(colored("You are on a Beta Release of ", 'green') + colored("BrainChaseSolver™ ", "blue"))
-                print(colored("=====================", 'magenta'))
-                print(colored("Current Version: ", 'blue') + colored(ver,'cyan'))
-                print(colored("Newest Version:  ", 'blue') + colored(new_version,'cyan'))
+                print(colored("===================================", 'magenta'))
+                print(colored("Current Version: ", 'blue') + colored(ver,'cyan') + colored("  (" + ver_date[0:2] + '/' + ver_date[2:4] + '/' + ver_date[4:8] + ")",'magenta'))
+                print(colored("Newest Version:  ", 'blue') + colored(new_version,'cyan') + colored("  (" + new_version_date[0:2] + '/' + new_version_date[2:4] + '/' + new_version_date[4:8] + ")",'magenta'))
             elif float(ver) < float(new_version):
                 print(colored("BrainChaseSolver™ ", "blue") + colored("is out of date...", 'red'))
-                print(colored("=====================", 'magenta'))
-                print(colored("Current Version: ", 'blue') + colored(ver,'cyan'))
-                print(colored("Newest Version:  ", 'blue') + colored(new_version,'cyan'))
+                print(colored("===================================", 'magenta'))
+                print(colored("Current Version: ", 'blue') + colored(ver,'cyan') + colored("  (" + ver_date[0:2] + '/' + ver_date[2:4] + '/' + ver_date[4:8] + ")",'magenta'))
+                print(colored("Newest Version:  ", 'blue') + colored(new_version,'cyan') + colored("  (" + new_version_date[0:2] + '/' + new_version_date[2:4] + '/' + new_version_date[4:8] + ")",'magenta'))
                 update_input = input(colored("Would You Like To Update? (Y/N) ", 'magenta'))
                 if update_input.lower() == 'y':
                     print(colored("BrainChaseSolver™ in Updating... ", "yellow"))
                     update_code = urllib.request.urlopen("https://raw.githubusercontent.com/Basicprogrammer10/Python/master/BrainChase/Index.py")
                     update_code_string = update_code.read().decode()
                     update_output = open('BrainChaseSolver_V' + new_version.replace('.','-') + '.py','w', encoding="utf-8")
-                    update_output.write(update_code_string)
+                    update_output.write("filepath = '" + str(Path(__file__).absolute()) + "'\n" + update_code_string)
                     print(colored("Done!", "green"))
                     print(colored('Run ', 'blue')+ colored('"BrainChaseSolver_V' + new_version.replace('.','-') + '.py"','cyan') + colored('to use V' + new_version, 'blue'))
                     quit()
@@ -258,8 +258,7 @@ else:
         command = user[0].lower()
 
         if command == 'version':
-            print(colored("BrainChaseSolver™", 'blue') + " | " + colored("V" +
-                                                                         str(ver), 'yellow') + " | " + colored("By: Connor Slade", 'green'))
+            print(colored("BrainChaseSolver™", 'blue') + " | " + colored("V" + str(ver), 'yellow') + " | " + colored(ver_date[0:2] + '/' + ver_date[2:4] + '/' + ver_date[4:8] ,'magenta') + " | " + colored("By: Connor Slade", 'green'))
 
         elif command == 'help':
             print(colored("Debug       Lets you run code           ;CodeToRun\nDecode      Decodes Caesar ciphers      ;Text;File\nDuck        Duckduckgos Something        ;CuteTurtle\nExit        Exits the Shell\nHelp        Shows this nice message\nNose        Noses you\nUn Mix      Unmixes a string            ;String;Auto\nUpdate      Updates the program\nVersion     Gives you Version Info", 'blue'))
@@ -288,28 +287,29 @@ else:
                     update_data = urllib.request.urlopen("https://raw.githubusercontent.com/Basicprogrammer10/Python/master/BrainChase/BrainChase.version")
                     config.read_string(update_data.read().decode())
                     new_version = config.get('version_info', 'new_version')
+                    new_version_date = config.get('version_info', 'update_date')
                     if float(ver) == float(new_version):
                         print(colored("BrainChaseSolver™ ", "blue") + colored("is up to date!", 'green'))
-                        print(colored("=====================", 'magenta'))
-                        print(colored("Current Version: ", 'blue') + colored(ver,'cyan'))
-                        print(colored("Newest Version:  ", 'blue') + colored(new_version,'cyan'))
+                        print(colored("===================================", 'magenta'))
+                        print(colored("Current Version: ", 'blue') + colored(ver,'cyan') + colored("  (" + ver_date[0:2] + '/' + ver_date[2:4] + '/' + ver_date[4:8] + ")",'magenta'))
+                        print(colored("Newest Version:  ", 'blue') + colored(new_version,'cyan') + colored("  (" + new_version_date[0:2] + '/' + new_version_date[2:4] + '/' + new_version_date[4:8] + ")",'magenta'))
                     elif float(ver) > float(new_version):
                         print(colored("You are on a Beta Release of ", 'green') + colored("BrainChaseSolver™ ", "blue"))
-                        print(colored("=====================", 'magenta'))
-                        print(colored("Current Version: ", 'blue') + colored(ver,'cyan'))
-                        print(colored("Newest Version:  ", 'blue') + colored(new_version,'cyan'))
+                        print(colored("===================================", 'magenta'))
+                        print(colored("Current Version: ", 'blue') + colored(ver,'cyan') + colored("  (" + ver_date[0:2] + '/' + ver_date[2:4] + '/' + ver_date[4:8] + ")",'magenta'))
+                        print(colored("Newest Version:  ", 'blue') + colored(new_version,'cyan') + colored("  (" + new_version_date[0:2] + '/' + new_version_date[2:4] + '/' + new_version_date[4:8] + ")",'magenta'))
                     elif float(ver) < float(new_version):
                         print(colored("BrainChaseSolver™ ", "blue") + colored("is out of date...", 'red'))
-                        print(colored("=====================", 'magenta'))
-                        print(colored("Current Version: ", 'blue') + colored(ver,'cyan'))
-                        print(colored("Newest Version:  ", 'blue') + colored(new_version,'cyan'))
+                        print(colored("===================================", 'magenta'))
+                        print(colored("Current Version: ", 'blue') + colored(ver,'cyan') + colored("  (" + ver_date[0:2] + '/' + ver_date[2:4] + '/' + ver_date[4:8] + ")",'magenta'))
+                        print(colored("Newest Version:  ", 'blue') + colored(new_version,'cyan') + colored("  (" + new_version_date[0:2] + '/' + new_version_date[2:4] + '/' + new_version_date[4:8] + ")",'magenta'))
                         update_input = input(colored("Would You Like To Update? (Y/N) ", 'magenta'))
                         if update_input.lower() == 'y':
                             print(colored("BrainChaseSolver™ in Updating... ", "yellow"))
                             update_code = urllib.request.urlopen("https://raw.githubusercontent.com/Basicprogrammer10/Python/master/BrainChase/Index.py")
                             update_code_string = update_code.read().decode()
                             update_output = open('BrainChaseSolver_V' + new_version.replace('.','-') + '.py','w', encoding="utf-8")
-                            update_output.write(update_code_string)
+                            update_output.write("filepath = '" + str(Path(__file__).absolute()) + "'\n" + update_code_string)
                             print(colored("Done!", "green"))
                             print(colored('Run ', 'blue')+ colored('"BrainChaseSolver_V' + new_version.replace('.','-') + '.py"','cyan') + colored('to use V' + new_version, 'blue'))
                             quit()
