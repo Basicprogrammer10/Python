5e853bbf442e1965b3310bd4a88c94487a47ef0b
Basicprogrammer10 <turtleslade@gmail.com>
2020-08-11 10:21:50 -0400

    oooooooooooooooooooo

---
+++ Z - STARTER.PY
@@ -0,0 +1,20 @@
+############ VARS ############
+initialise = {}
+toImport  =  {}
+########### SETUP ###########
+for i in toImport:
+    defult = False if toImport[i] != '' else True
+    exec(str('from ' if defult == False else 'import ')+str(i)+str(' import ' + toImport[i] if defult == False else ''))
+def colored(text, color):
+    ColorCodes = {'black':'30','red':'31','yellow':'33','green':'32','blue':'34','cyan':'36','magenta':'35','white':'37','gray':'90','reset':'0'}
+    return '\033[' + ColorCodes[str(color).lower()] + 'm' + str(text) + "\033[0m"
+for i in initialise:
+    exec(i + '=' + initialise[i])
+######### FUNCTIONS #########
+def function():
+    pass
+####### MAIN FUNCTION #######
+def main():
+    pass
+if __name__ == "__main__":
+    main()
