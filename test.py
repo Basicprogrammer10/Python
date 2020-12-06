8e1153aa88ba2a2ece037d68cb4cf4f62dcd09f6
Connor Slade <turtleslade@gmail.com>
2020-12-06 01:31:18 -0500

    i dont even know at this point...

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ for i in toImport:
     defult = False if toImport[i] != '' else True
     exec(str('from ' if defult == False else 'import ')+str(i)+str(' import ' + toImport[i] if defult == False else ''))
 def colored(text, color):
-    ColorCodes = {'black':'30','red':'31','yellow':'33','green':'32','blue':'34','cyan':'36','magenta':'35','white':'37','gray':'90','reset':'0'}
+    ColorCodes = {'black':'30','red':'31','yellow':'33','orange':'93','green':'32','blue':'34','cyan':'36','magenta':'35','white':'37','gray':'90','reset':'0'}
     return '\033[' + ColorCodes[str(color).lower()] + 'm' + str(text) + "\033[0m"
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -27,4 +27,7 @@ def main():
     print(colored("Server stopped.",'red'))
 
 if __name__ == "__main__":
-    main()
+    #main()
+    print(colored("■",'red'))
+    print(colored("■",'yellow'))
+    print(colored("■",'green'))
